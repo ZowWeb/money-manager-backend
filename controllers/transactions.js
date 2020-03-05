d0b837ba4b95fc1f4f403a1dfcc00227aec23275
ZowWeb <wrongzoayb@gmail.com>
2020-03-06 02:40:33 +0530

    Set up controllers to add and delete transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send("GET transactions");
 };
+
+// @desc    Add transaction
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transaction");
+};
+
+// @desc    Delete transactions
+// @route   DELETE /api/v1/transactions
+// @access  Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transactions");
+};

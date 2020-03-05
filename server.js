bf6f4193e5dde0adcf560d0524a8349e5de2db8c
ZowWeb <wrongzoayb@gmail.com>
2020-03-06 02:15:51 +0530

    Initial server setup and git repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+
+dotenv.config({ path: "./config/config.env" });
+
+// Start app
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
